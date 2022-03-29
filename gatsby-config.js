07742eb717ce88e94eb1b9b5910f464a54ba7beb
tschanadam <tschanadam@gmail.com>
2022-03-29 11:12:04 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://www.kattschan.xyz/`,
     // Your Name
     name: 'Kat Tschan',
     // Main Site Title
